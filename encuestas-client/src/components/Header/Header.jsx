f9544f60cc50a9f3327abdc57d874380f345945b
Daniels35 <danielstiven35@gmail.com>
2023-09-14 18:04:41 -0500

    Últimas mejoras estilos

--- encuestas-client/src/components/Header/Header.jsx
+++ encuestas-client/src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
   return (
     <header className="header">
-
+      <div className='header-container-link'>
+        <Link className='header-link' to="/encuestasHenry">
+        </Link>
+      </div>
     </header>
   );
 }
